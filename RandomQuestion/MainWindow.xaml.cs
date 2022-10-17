d14d40ce86401d9a454ecd4288fa08e3adce4be8
cristina313 <cristina.if890@gmail.com>
2022-10-17 16:05:06 +0300

    Added Win UI3 project to RandomQuestion solution

--- RandomQuestion/MainWindow.xaml.cs
+++ RandomQuestion/MainWindow.xaml.cs
@@ -1,161 +1,36 @@
-﻿using RandomQuestion.Classes;
-using RandomQuestion.Classes.DataManagement;
-using RandomQuestion.Models;
-using Serilog;
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel;
+using System.IO;
 using System.Linq;
-using System.Threading;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Media.Animation;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace RandomQuestion
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// An empty window that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public partial class MainWindow : Window
+    public sealed partial class MainWindow : Window
     {
-        ArrayList shuffledQuestions;
-        int currentQ;
-        private static BackgroundWorker backgroundWorker;
-        private IDataManager dataManager;
-        private readonly ILogger logger;
-
         public MainWindow()
         {
-            var databaseSettings = new DatabaseSettings();
-            var fileSetting = new FileSettings();
-           /* logger = new LoggerConfiguration()
-                .WriteTo.MongoDB(databaseSettings.ConnectionString)
-                .CreateLogger();*/
-
-            try
-            {
-                InitializeComponent();
-                dataManager = new FileManager(fileSetting); //new DatabaseManager(databaseSettings);
-
-                Init();
-
-                backgroundWorker = new BackgroundWorker
-                {
-                    WorkerReportsProgress = true,
-                    WorkerSupportsCancellation = true
-                };
-                backgroundWorker.DoWork += backgroundWorker_DoWork;
-                backgroundWorker.ProgressChanged += backgroundWorker_ProgressChanged;
-                backgroundWorker.RunWorkerCompleted += backgroundWorker_RunWorkerCompleted;
-            } 
-            catch (Exception ex)
-            {
-                logger?.Error($"Uncaught exceptions was thrown. Message: {ex.Message}");
-            }
+            this.InitializeComponent();
         }
 
-        private void backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        private void myButton_Click(object sender, RoutedEventArgs e)
         {
-            Blink(lblMain, 300, 3);
-            if (currentQ < shuffledQuestions.Count)
-            {
-                lblQuestion.Text = shuffledQuestions[currentQ].ToString();
-                currentQ++;
-            }
-            else
-            {
-                currentQ = 0;
-                Init();
-            }
-        }
-
-        private void backgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
-        {
-            lblQuestion.Text = e.UserState.ToString();
-        }
-
-        private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
-        {
-            int nrQ = 20; 
-            int sec = 1;
-            var subArr = ShuffleManager.PickUpSubArray(shuffledQuestions, nrQ);
-            
-            foreach (var q in subArr)
-            {
-                Thread.Sleep(7 * sec);
-                backgroundWorker.ReportProgress(sec*(100/nrQ),q);
-                sec++;
-                
-            }
-        }
-
-        private void Init()
-        {
-            try
-            {
-                /*IEnumerable < Question > questions = new List<Question>()
-                {
-                    new Question() {Text = "DSSDFS"},
-                    new Question() {Text = "DSSDFS"}
-                };
-
-                ArrayList list = (ArrayList)questions.Select(q => q.Text);
-                */
-                logger?.Information("Trying to read questions...");
-                var gotQuestions = dataManager.ReadQuestions();
-                lblQuestion.Text = "Hit that button :>";
-
-                if(gotQuestions.Count == 0)
-                {
-                    MessageBox.Show($"Auleu! Bagî șî tu niști întrebări la baza di dati!", "Mesaj de baraj", MessageBoxButton.OK, MessageBoxImage.Exclamation);
-                    return;
-                }
-
-                shuffledQuestions = ShuffleManager.Shuffle(gotQuestions);
-                logger?.Information($"{gotQuestions.Count} questions were read!");
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Ioi no'! Sigur ai pus fisieru' unde trăbă? Uită ni ce zice programu': {ex.Message}","Mesaj de baraj",MessageBoxButton.OK,MessageBoxImage.Exclamation);
-                logger?.Error($"Question read error. Message: {ex.Message}");
-            }
-        }
-
-        private void btnHit_Click(object sender, RoutedEventArgs e)
-        {
-            if (shuffledQuestions == null) 
-            {
-                MessageBox.Show("Apăi nicio întrebare nu s-o loadat! Verifică te rog amu' fișieru' cu întrebări!");
-                logger?.Error("Get next question button was pressed, but questions were not previously loaded!");
-                return;
-            }
-
-            backgroundWorker.RunWorkerAsync();
-        }
-
-        private void Blink(Label label, int length, int times)
-        {
-            
-
-            var doubleAnimation = new DoubleAnimation
-            {
-                From = 1.0,
-                To = 0.0,
-                Duration = new Duration(TimeSpan.FromMilliseconds(length)),
-                AutoReverse = true,
-                RepeatBehavior = new RepeatBehavior(times)
-            };
-            Storyboard storyBoard = new Storyboard();
-            storyBoard.Children.Add(doubleAnimation);
-            Storyboard.SetTarget(doubleAnimation, label);
-            Storyboard.SetTargetProperty(doubleAnimation, new PropertyPath("Opacity"));
-            storyBoard.Begin(label);
-        }
-
-        private void closeButton_Click(object sender, RoutedEventArgs e)
-        {
-            Close();
+            myButton.Content = "Clicked";
         }
     }
 }
