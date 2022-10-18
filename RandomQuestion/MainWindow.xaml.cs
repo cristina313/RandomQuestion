d65067b9a9ae9b5a3da2f5f8ad2d0a5be1bfb6c2
cristina313 <cristina.if890@gmail.com>
2022-10-18 08:37:56 +0300

    Added logo and background image assets Added country flags for languages Added main window and integrated design assets

--- RandomQuestion/MainWindow.xaml.cs
+++ RandomQuestion/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
 ﻿using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using RandomQuestion.Utils;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -26,11 +14,9 @@ namespace RandomQuestion
         public MainWindow()
         {
             this.InitializeComponent();
-        }
-
-        private void myButton_Click(object sender, RoutedEventArgs e)
-        {
-            myButton.Content = "Clicked";
+            this.Title = Constants.TITLE;
+            ExtendsContentIntoTitleBar = true;
+            SetTitleBar(TitleBar);
         }
     }
 }
