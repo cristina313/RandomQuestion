d1481f3d0b59a8888c94341d7d1dee6729c6ff7a
cristina313 <cristina.if890@gmail.com>
2022-10-19 21:03:03 +0300

    Added shuffle mechanism

---
+++ RandomQuestion/Utils/Shuffling/ShuffleManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+
+namespace RandomQuestion.Utils.Shuffling
+{
+    public class ShuffleManager
+    {
+        private static Random random = new Random();
+
+        public static ArrayList Shuffle(ArrayList array)
+        {
+            for (int index = 0; index < 1000; index++)
+            {
+                var first = random.Next(array.Count);
+                var second = random.Next(array.Count);
+
+                var temp = array[first];
+                array[first] = array[second];
+                array[second] = temp;
+            }
+            return array;
+        }
+    }
+}
